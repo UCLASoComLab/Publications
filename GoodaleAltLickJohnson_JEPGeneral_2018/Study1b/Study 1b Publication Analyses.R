442ef49d528d8707d92b66c00b7b0a6b6bb04630
Brianna Goodale <bgoodale@post.harvard.edu>
2017-12-10 16:46:42 -0700

    Added Study1b final analyses

--- GoodaleAltLickJohnson_JEPGeneral_2018/Study1b/Study 1b Publication Analyses.R
+++ GoodaleAltLickJohnson_JEPGeneral_2018/Study1b/Study 1b Publication Analyses.R
@@ -7,7 +7,7 @@ library(cowplot)
 library(ggplot2)
 
 #Read in Data
-masterpptlist <- read.csv("~/Dropbox/Research/Ensemble Coding/Study 3 or 1B Replicating Study 1/Redo Study 3/Data Analysis/Final Publication Analyses & Data/Goodale_Data_Study1b.csv",
+masterpptlist <- read.csv("~/repos/Publications/GoodaleAltLickJohnson_JEPGeneral_2018/Study1b/Goodale_Data_Study1b.csv",
            header=TRUE)
 masterpptlist <- as.data.frame(masterpptlist)
 
@@ -97,7 +97,6 @@ mod3.new <- data.frame(
   , tlo = mod3.new$Ratio-cmult*sqrt(tvar1)
   , thi = mod3.new$Ratio+cmult*sqrt(tvar1)
 )
-library(cowplot)
 #plot confidence intervals
 g0 <- ggplot(mod3.new, aes(x=Condition, y=Ratio, colour=Gender))
 g0 + geom_ribbon(aes(ymin = plo, ymax = phi, linetype=NA), alpha=0.2, show.legend=FALSE)+
@@ -124,25 +123,10 @@ summary(mod1)
 # calculate ICC by summing everything in variance column & dividing participant variance by it
 5.956/(5.956+11.743)
 
-mod2 <- lmer(AverageFace~Condition + (1|Participant), data=masterpptlist)
-summary(mod2)
-
-mod3 <- lmer(AverageFace~Condition*Gender+ (1|Participant), data=masterpptlist)
-summary(mod3)
-
-#random effects & random intercepts#
-
 mod3b <- lmer(AverageFace~Condition*Gender + (1+Condition|Participant), data=masterpptlist)
 summary(mod3b)
 multilevel.effect.size(mod3b)
 
-anova(mod3, mod3b)
-
-mod3b <- lmer(AverageFace~Condition+Gender + (Condition|Participant), data=masterpptlist)
-summary(mod3b)
-anova(mod3, mod3b)
-
-#Sig difference, so need to use random slopes, random effects#
 mod3.new <- with(masterpptlist, data.frame(Gender = factor(c(rep(0,5),rep(1,5))), Condition = c(0,3,6,9,12,0,3,6,9,12)))
 mod3.new$AverageFace <- NULL
 mod3.new$AverageFace <- 0
@@ -186,22 +170,9 @@ summary(mod1)
 # calculate ICC by summing everything in variance column & dividing participant variance by it
 0.247/(0.247+0.7622)
 
-mod2 <- lmer(NormAvg~Condition + (1|Participant), data=masterpptlist)
-summary(mod2)
-
-mod3 <- lmer(NormAvg~Condition*Gender+ (1|Participant), data=masterpptlist)
-summary(mod3)
-
-#random effects & random intercepts#
-
 mod3b <- lmer(NormAvg~Condition*Gender + (1+Condition|Participant), data=masterpptlist)
 summary(mod3b)
 multilevel.effect.size(mod3b)
-anova(mod3, mod3b)
-
-mod3b <- lmer(NormAvg~Condition + Gender+(Condition|Participant), data=masterpptlist)
-summary(mod3b)
-anova(mod3, mod3b)
 
 #Sig difference, so need to use random slopes, random effects#
 mod3.new <- with(masterpptlist, data.frame(Gender = factor(c(rep(0,5),rep(1,5))), Condition = c(0,3,6,9,12,0,3,6,9,12)))
@@ -246,30 +217,21 @@ summary(mod1)
 # calculate ICC by summing everything in variance column & dividing participant variance by it
 1.156/(1.156+2.929)
 
-mod2 <- lmer(FitBelong~Condition+Gender+ (Condition|Participant), data=masterpptlist)
-summary(mod2)
-
-mod3 <- lmer(FitBelong~Condition*Gender+ (1|Participant), data=masterpptlist)
-summary(mod3)
-
-#random effects & random intercepts#
-
 mod3b <- lmer(FitBelong~Condition*Gender + (1+Condition|Participant), data=masterpptlist)
 summary(mod3b)
 
 multilevel.effect.size(mod3b)
-#finding power analysis#
-
-fixef(mod3b)
-mod3c <- lmer(FitBelong~Condition*RCGender + (1+Condition|Participant), data=masterpptlist)
-summary(mod3c)
-
-anova(mod3b, mod3c)
-#Sig difference, so need to use random slopes, random effects#
 
 #Reversing coding so can see simple slopes for women as comparison group#
 mod3 <- lmer(FitBelong~Condition*RCGender+ (Condition|Participant), data=masterpptlist)
 summary(mod3)
+
+#Simple effects analysis#
+mod3a <- lmer(FitBelong~Condition + (Condition|Participant), data=women.data)
+summary(mod3a)
+
+mod3b <- lmer(FitBelong~Condition+ (Condition|Participant), data=men.data)
+summary(mod3b)
 
 mod3.new <- with(masterpptlist, data.frame(Gender = factor(c(rep(0,5),rep(1,5))), 
                                            Condition = c(0,3,6,9,12,0,3,6,9,12)))
@@ -305,25 +267,3 @@ g0 + geom_ribbon(aes(ymin = plo, ymax = phi, linetype=NA), alpha=0.2, show.legen
   coord_cartesian(ylim= c(1, 7), xlim = c(0, 12))+
   theme(legend.justification=c(1,0.1), legend.position=c(1,0.1), legend.text=element_text(size=16), 
         legend.title=element_text(size=20), axis.title=element_text(size=20), axis.text=element_text(size=16))
-
-
-mod3a <- lmer(FitBelong~Condition + (Condition|Participant), data=women.data)
-summary(mod3a)
-
-mod3b <- lmer(FitBelong~Condition+ (Condition|Participant), data=men.data)
-summary(mod3b)
-
-mod3c <- lmer(FitBelong~Gender+ (1|Participant), data= Zeromen.data)
-summary(mod3c)
-
-mod3d <- lmer(FitBelong~Gender+ (1|Participant), data= TwentyFivemen.data)
-summary(mod3d)
-
-mod3e <- lmer(FitBelong~Gender+ (1|Participant), data= Fiftymen.data)
-summary(mod3e)
-
-mod3f <- lmer(FitBelong~Gender+ (1|Participant), data= SeventyFivemen.data)
-summary(mod3f)
-
-mod3g <- lmer(FitBelong~Gender+ (1|Participant), data= Onehundredmen.data)
-summary(mod3g)
